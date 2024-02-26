db08dfa0b688af573b99b883a81e20f47b6468c8
TheTimeSweeper <leo.midget@gmail.com>
2024-02-25 23:18:12 -0800

    example simple outfit

---
+++ ExampleSimpleOutfit/ExampleSimpleOutfit.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using LegendAPI;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ExampleSimpleOutfit {
+
+    [BepInDependency("TheTimeSweeper.CustomPalettes", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("TheTimeSweeper.ExampleSimpleOutfit", "ExampleSimpleOutfit", "0.1.0")]
+    public class ExampleSimpleOutfit : BaseUnityPlugin {
+        void Awake() {
+
+            int newColorIndex = CustomPalettes.Palettes.AddPalette(System.IO.Path.GetDirectoryName(Info.Location), "example.png");
+
+            OutfitInfo coolOutfit = new OutfitInfo()
+            {
+                name = "Cool Guy Colored",
+                outfit = new Outfit(
+                    "ModName::ExampleOutfitID", //newID
+                    newColorIndex, //newColorIndex
+                    new List<OutfitModStat>
+                    {
+                        new OutfitModStat(OutfitModStat.OutfitModType.Health,
+                              100, //addValue
+                              0, //multiValue
+                              0, //override value
+                              false), //bool value
+                        // more OutfitModStats here
+                    },
+                    false, //isUnlocked
+                    false) //isLeveling
+            };
+            LegendAPI.Outfits.Register(coolOutfit);
+        }
+    }
+}
---
+++ ExampleSimpleOutfit/Release/README.md
@@ -0,0 +1,15 @@
+# ExampleSimpleOutfit
+we thunderstorin now
+
+Any questions or feedback or mind exploding issues, ping/message `thetimesweeper` on Discord
+___
+
+### Installation (manual):
+- Make sure you have the dependencies installed.
+- Download and extract the zip.
+- Place the .dll file into your BepInEx `plugins` folder
+
+### Changelog:
+
+`0.1.0`
+ - c:
